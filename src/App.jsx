182ffb8103479d36d9e265d4fd35619d255b50fb
gitlad2 <dalton.m.nesmith2@outlook.com>
2022-05-25 10:42:14 -0400

    -Added a search box and imported main to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/header/Header";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import Main from "./Pages/Main"
 import Navbar from "./Components/Navbar";
 
 function App() {
 	return (
 		<>
 			<div className="app">
-				{/* <Navbar /> */}
 				<Header />
 
 				<Router>
+				<Navbar />
 					<Routes>
 						<Route path="/" element={<About />} />
 						<Route path="/Contact" element={<Contact />} />
+						<Route path="/Main" element={<Main />} />
 					</Routes>
 				</Router>
 			</div>
