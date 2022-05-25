130eeead4529517ac9bcaeb866e7eb7198703654
Bradley Wilson <bwils35@gmail.com>
2022-05-25 08:53:08 -0500

    Create workflow
    remove unnecessary files and install react-router-dom to create a boilerplate app

---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header = () => {
+	return (
+		<>
+			<header>
+				<div className="header__container">
+					<h5>Welcome to...</h5>
+				</div>
+			</header>
+		</>
+	);
+};
+
+export default Header;
