5ca4c3973cec7ad9bdebe1ebbf4a35fa8c238441
Bradley Wilson <bwils35@gmail.com>
2022-05-25 15:19:25 -0500

    Fix errors in About.jsx

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,52 +1,52 @@
 import React, { Component } from "react";
 import {
-    Modal,
-    ModalContent,
-    SubmitButton,
+	Modal,
+	ModalContent,
+	SubmitButton,
 } from "../Styles/Containers/Modal.style";
 import Select from "react-select";
 
 const submitHandler = (e) => {
-    e.preventDefault();
+	e.preventDefault();
 };
 
 const options = [
-    { value: "Crystals Shit", label: "Crystals" },
-    { value: "Magic", label: "Magic BS" },
-    { value: "Hippy", label: "Hippy Dust" },
+	{ value: "Crystals Shit", label: "Crystals" },
+	{ value: "Magic", label: "Magic BS" },
+	{ value: "Hippy", label: "Hippy Dust" },
 ];
 
 const ComboBox = () => <Select options={options} />;
 
 function Main() {
-    return (
-        <>
-            <div>
-                <title className="title">Krystal Magic!</title>
-                <main className="main">
-                    Welcome to the Store.
-                    <p className="par">
-                        <h4>Select a Category</h4>
-                        <ComboBox></ComboBox>
-                        Search our products:
-                        <form margin="10px" onSubmit={submitHandler}>
-                            <input
-                                type="text"
-                                onChange={(e) => e.target.value}
-                            />
-                            <button classname="searchButton" type="submit">
-                                Search
-                            </button>
-                        </form>
-                    </p>
-                </main>
-            </div>
-            <Modal>
-                <ModalContent>Product Place Holder</ModalContent>
-                <SubmitButton>Purchase!</SubmitButton>
-            </Modal>
-        </>
-    );
+	return (
+		<>
+			<div>
+				<title className="title">Krystal Magic!</title>
+				<main className="main">
+					Welcome to the Store.
+					<p className="par">
+						<h4>Select a Category</h4>
+						<ComboBox></ComboBox>
+						Search our products:
+						<form margin="10px" onSubmit={submitHandler}>
+							<input
+								type="text"
+								onChange={(e) => e.target.value}
+							/>
+							<button classname="searchButton" type="submit">
+								Search
+							</button>
+						</form>
+					</p>
+				</main>
+			</div>
+			<Modal>
+				<ModalContent>Product Place Holder</ModalContent>
+				<SubmitButton>Purchase!</SubmitButton>
+			</Modal>
+		</>
+	);
 }
 
 export default Main;
