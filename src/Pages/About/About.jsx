5cad344963207f216e66ea1f3c199a72106f5dba
Tre Willis <deantrewillis00@gmail.com>
2022-05-26 15:24:48 -0500

    Added picturecarousel

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import Carousel from "react-elastic-carousel"
 import "./About.css"
+import CrystalPic from "../../Images/CrystalPic.jpg"
+import SagePic from "../../Images/SagePic.jpg"
+import IncensePic from "../../Images/IncensePic.jpg"
+import ClothingPic from "../../Images/ClothingPic.jpg"
+
+
+const breakPoints = [
+	{ width: 1, itemsToShow: 1 },
+	{ width: 550, itemsToShow: 2 },
+	{ width: 768, itemsToShow: 3 },
+	{ width: 1200, itemsToShow: 4 },
+]
 
 function About() {
 	return (
 		<>
-			<div className="view">About</div>
+			<div className="view">
+				<div className="">
+					<ul className="categories">
+						<li><a href="">Jewelery</a></li>
+						<li><a href="">Crystal's</a></li>
+						<li><a href="">Sage</a></li>
+						<li><a href="">Incense</a></li>
+						<li><a href="">Clothing</a></li>
+						<li><a href="">Hot Offers</a></li>
+					</ul>
+				</div>
+				<div className="image_carousel">
+					<Carousel breakpoints={breakPoints}>
+						<img src={CrystalPic} alt="firstpics" className="pic_one" />
+						<img src={SagePic} alt="firstpics" className="pic_one" />
+						<img src={IncensePic} alt="firstpics" className="pic_one" />
+						<img src={ClothingPic} alt="firstpics" className="pic_one" />
+					</Carousel>
+				</div>
+			</div>
 		</>
 	);
 }
