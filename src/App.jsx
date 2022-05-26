4c796374c024f90ffadf75ec6bde16be1fb37b44
Tre Willis <deantrewillis00@gmail.com>
2022-05-26 13:49:04 -0500

    Page orginization

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/header/Header";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
-import Main from "./Pages/Main";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Main from "./Pages/Main/Main";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/footer/footer";
 
@@ -14,8 +14,6 @@ function App() {
 				<Router>
 					<Navbar />
 					<Header />
-
-					<h1>jkaslndkjasnd</h1>
 					<Routes>
 						<Route path="/" element={<About />} />
 						<Route path="/Contact" element={<Contact />} />
